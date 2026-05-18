48b0255c1248baa95213e3d4afd61d22daa8758e
Thibaut CHAUFFIER <tchauffi@gmail.com>
2026-05-18 22:25:20 +0200

    Update dependancies

---
+++ src/talking_vit/trainers/__init__.py
@@ -0,0 +1,4 @@
+from talking_vit.trainers.dataset import HFImageCaptionDataset
+from talking_vit.trainers.trainer import TrainConfig, Trainer
+
+__all__ = ["HFImageCaptionDataset", "TrainConfig", "Trainer"]
